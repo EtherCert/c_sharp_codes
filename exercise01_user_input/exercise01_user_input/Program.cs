d457fa160846347f935ebbc0fc63da4624a2f9ca
Samia <samiamhassouna@gmail.com>
2020-10-05 02:54:17 +0300

    exercise01_user_input

---
+++ exercise01_user_input/exercise01_user_input/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace exercise01_user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your age:");
+            string age = Console.ReadLine();
+            Console.WriteLine("Enter your phone:");
+            string phone = Console.ReadLine();
+            Console.WriteLine("Enter your address:");
+            string address = Console.ReadLine();
+            Console.WriteLine("-+-+-+-+ Your Info -+-+-+-+-\nName: " + name + "\nAge: " + age + "\nphone: " + phone + "\naddress: " + address + "\n-+-+-+-+-+-+-+-+-+-+-+-+-+-");
+        }
+    }
+}
+
+
