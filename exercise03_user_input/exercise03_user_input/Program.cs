c223600902962bc4e53f7f8c65cd6e9c48c23855
Samia <samiamhassouna@gmail.com>
2020-10-05 04:09:05 +0300

    exercise03_user_input

---
+++ exercise03_user_input/exercise03_user_input/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+اكتبي برنامج يسمح للمستخدم بإدخال طول وعرض المستطيل ثم احسبي:
+مساحة المستطيل (الطول * العرض)
+محيط المستطيل  2*(الطول + العرض)
+*/
+using System;
+namespace exercise03_user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Width ..");
+            double width = double.Parse(Console.ReadLine());//استقبال العرض
+            Console.WriteLine("Enter length ..");
+            double length = double.Parse(Console.ReadLine());//استقبال الطول
+            
+            double area = width * length; // حساب مساحة المستطيل
+            double perimeter = 2 * (width + length); // حساب محيط المستطيل
+
+            Console.WriteLine("Area = "+area+ "\nPerimeter = "+ perimeter);// طباعة النتائج
+
+        }
+    }
+}
