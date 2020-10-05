890a0b43ab7b959edc25436f6cfb7385545ceda3
Samia <samiamhassouna@gmail.com>
2020-10-05 03:48:22 +0300

    exercise02_user_input

---
+++ exercise02_user_input/exercise02_user_input/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+اكتبي برنامج يسمح للمستخدم بإدخال 5 أرقام من نوع double ؛ ثم 
+جدي مجموع ومتوسط الأرقام(مجموع القيم / عددها).
+*/
+using System;
+namespace exercise02_user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter five numbers, please ...");
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            double n3 = double.Parse(Console.ReadLine());
+            double n4 = double.Parse(Console.ReadLine());
+            double n5 = double.Parse(Console.ReadLine());
+
+            double sum = n1 + n2 + n3 + n4 + n5;
+            double avg = sum / 5;
+            Console.WriteLine("Sum = "+sum+"\nAvarage = "+avg);
+            
+        }
+    }
+}
